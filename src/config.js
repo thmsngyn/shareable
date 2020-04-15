d55bf2891f0d04e7a2cf8f69250c5b8a679a022b
Tom Nguyen <tomtronwin@gmail.com>
2020-04-14 21:06:16 -0700

    Use spotify and storage services in main component

--- src/config.js
+++ src/config.js
@@ -1,11 +1,4 @@
-
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "REDACTED_SPOTIFY_CLIENT_ID";
-export const redirectUri = "http://localhost:3000/redirect";
-export const scopes = [
-    "user-top-read",
-    "user-read-currently-playing",
-    "user-read-playback-state",
-];
+export const clientId = 'REDACTED_SPOTIFY_CLIENT_ID';
+export const redirectUri = 'http://localhost:3000/redirect';
+export const scopes = ['user-top-read', 'user-read-currently-playing', 'user-read-playback-state'];
