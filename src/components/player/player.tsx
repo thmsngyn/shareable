9c9f23557d1e2ba5641a16806d5d1d9a0f9d282d
Tom Nguyen <tomtronwin@gmail.com>
2020-04-14 20:15:55 -0700

    Convert files to typescript files

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import "./player.css";
+import React from 'react';
+import './player.css';
 
-const Player = props => {
+const Player = (props: any) => {
   const backgroundStyles = {
-    backgroundImage:`url(${
-      props.item.album.images[0].url
-    })`,
+    backgroundImage: `url(${props.item.album.images[0].url})`,
   };
 
   const progressBarStyles = {
-    width: (props.progress_ms * 100 / props.item.duration_ms) + '%'
+    width: (props.progress_ms * 100) / props.item.duration_ms + '%',
   };
 
   return (
@@ -20,20 +18,16 @@ const Player = props => {
         </div>
         <div className="now-playing__side">
           <div className="now-playing__name">{props.item.name}</div>
-          <div className="now-playing__artist">
-            {props.item.artists[0].name}
-          </div>
-          <div className="now-playing__status">
-            {props.is_playing ? "Playing" : "Paused"}
-          </div>
+          <div className="now-playing__artist">{props.item.artists[0].name}</div>
+          <div className="now-playing__status">{props.is_playing ? 'Playing' : 'Paused'}</div>
           <div className="progress">
             <div className="progress__bar" style={progressBarStyles} />
           </div>
         </div>
-        <div className="background" style={backgroundStyles} />{" "}
+        <div className="background" style={backgroundStyles} />{' '}
       </div>
     </div>
   );
-}
+};
 
 export default Player;
