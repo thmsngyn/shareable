f48bca81ed2c491a3f5f683833001bf0fa804db5
Tom Nguyen <tomtronwin@gmail.com>
2020-04-17 00:15:12 -0700

    Clean dis shit up

--- src/App.tsx
+++ src/App.tsx
@@ -2,72 +2,52 @@ import React from 'react';
 
 import { hot } from 'react-hot-loader/root';
 
-import './App.css';
-import { clientId, redirectUri, scopes } from './config';
-import hash from './utils/hash';
-import Player from './components/player/player';
 import logo from './logo.svg';
-import { SpotifyService, AUTH_API } from './services';
-import { StorageService } from './services/storage';
-import { StorageKeys } from './services/storage/storage.constants';
+import { Login, Player } from './components';
+import { SpotifyService, CurrentPlayback } from './services';
+
+import './App.css';
 
 interface AppProps {}
 
 interface AppState {
-  token: string | null;
   item: any;
-  is_playing: string;
+  is_playing: boolean;
   progress_ms: number;
+  loggedIn: boolean;
 }
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = {
-      token: null,
       item: {
         album: {
-          images: [{ url: '' }],
+          images: [{ url: '', height: 0, width: 0 }],
         },
         name: '',
         artists: [{ name: '' }],
         duration_ms: 0,
       },
-      is_playing: 'Paused',
+      is_playing: true,
       progress_ms: 0,
+      loggedIn: false,
     };
   }
-  componentDidMount() {
-    this.setUserToken();
-  }
 
-  setUserToken() {
-    // Set token
-    let token = hash.access_token;
-    const tokenFromStorage = StorageService.get(StorageKeys.SpotifyToken);
+  async componentDidMount() {
+    if (SpotifyService.resolveUserToken()) {
+      this.setState({ loggedIn: true });
 
-    if (token || tokenFromStorage) {
-      token = token || tokenFromStorage;
-
-      if (!tokenFromStorage) {
-        StorageService.set(StorageKeys.SpotifyToken, token);
-        StorageService.setExpiration(StorageKeys.SpotifyToken, 36000000); // 1 hr
-      } else {
-        StorageService.checkExpiration();
-      }
-
-      // Set token
-      this.setState({ token });
-      SpotifyService.getCurrentlyPlaying(token, this.setCurrentlyPlaying.bind(this));
+      const currentlyPlaying: CurrentPlayback = await SpotifyService.getCurrentlyPlaying();
+      this.setState({
+        item: currentlyPlaying.item,
+        is_playing: currentlyPlaying.is_playing,
+        progress_ms: currentlyPlaying.progress_ms,
+      });
+    } else {
+      this.setState({ loggedIn: false });
     }
-  }
-
-  setCurrentlyPlaying(data: any) {
-    this.setState({
-      item: data.item,
-      is_playing: data.is_playing,
-      progress_ms: data.progress_ms,
-    });
   }
 
   render() {
@@ -75,17 +55,8 @@ class App extends React.Component<AppProps, AppState> {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {!this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${AUTH_API}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                '%20'
-              )}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-            </a>
-          )}
-          {this.state.token && (
+          {!this.state.loggedIn && <Login></Login>}
+          {this.state.loggedIn && (
             <Player item={this.state.item} is_playing={this.state.is_playing} progress_ms={this.state.progress_ms} />
           )}
         </header>
--- src/services/spotify/spotify.service.tsx
+++ src/services/spotify/spotify.service.tsx
@@ -1,19 +1,53 @@
-import * as $ from 'jquery';
+import hash from '../../utils/hash';
+import { StorageService, StorageKeys } from '../storage';
 
-import { PLAYER_API } from './spotify.constants';
+import { PLAYER_API, SpotifyErrorMessage } from './spotify.constants';
+import { CurrentPlayback } from './spotify.types';
 
 export const SpotifyService = new (class {
+  token: string = '';
   constructor() {}
 
-  getCurrentlyPlaying(token: string, stateHandler: Function) {
-    // Make a call using the token
-    $.ajax({
-      url: PLAYER_API,
-      type: 'GET',
-      beforeSend: (xhr: any) => {
-        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-      },
-      success: (success) => stateHandler(success),
-    });
+  resolveUserToken(): string {
+    // Set token
+    this.token = hash.access_token;
+    const tokenFromStorage = StorageService.get(StorageKeys.SpotifyToken);
+
+    if (!!tokenFromStorage || !!this.token) {
+      this.token = tokenFromStorage || this.token;
+
+      if (!tokenFromStorage) {
+        StorageService.set(StorageKeys.SpotifyToken, this.token);
+        StorageService.setExpiration(StorageKeys.SpotifyToken, 36000000); // 1 hr
+      } else {
+        StorageService.checkExpiration();
+      }
+
+      return this.token;
+    }
+    return '';
   }
+
+  getCurrentlyPlaying(): Promise<CurrentPlayback> {
+    // Make a call using the token
+    return fetch(PLAYER_API, {
+      method: 'GET',
+      headers: new Headers({
+        Authorization: 'Bearer ' + this.token,
+      }),
+    })
+      .then((response) => response.json())
+      .then((currentPlaying: CurrentPlayback) => {
+        const { error } = currentPlaying;
+        if (error) {
+          switch (error.message) {
+            case SpotifyErrorMessage.TokenExpired:
+              StorageService.remove(StorageKeys.SpotifyToken);
+          }
+        }
+        return currentPlaying;
+      });
+  }
+
+  // getLikes(token: string, )
 })();
