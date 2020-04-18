e269b3b549dfbb107eab920c3b44a5cef5449422
Tom Nguyen <tomtronwin@gmail.com>
2020-04-17 22:49:15 -0700

    Add user-library-read scope

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = 'REDACTED_SPOTIFY_CLIENT_ID';
 export const redirectUri = 'http://localhost:3000/redirect';
-export const scopes = ['user-top-read', 'user-read-currently-playing', 'user-read-playback-state'];
+export const scopes = ['user-top-read', 'user-read-currently-playing', 'user-read-playback-state', 'user-library-read'];
