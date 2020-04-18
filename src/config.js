ac64fbdd614a18b05397750f54d55b410f04ae95
Tom Nguyen <tomtronwin@gmail.com>
2020-04-18 00:34:04 -0700

    Add web playback sdk

--- src/config.js
+++ src/config.js
@@ -1,4 +1,12 @@
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = 'REDACTED_SPOTIFY_CLIENT_ID';
 export const redirectUri = 'http://localhost:3000/redirect';
-export const scopes = ['user-top-read', 'user-read-currently-playing', 'user-read-playback-state', 'user-library-read'];
+export const scopes = [
+  'user-top-read',
+  'user-read-currently-playing',
+  'user-read-playback-state',
+  'user-library-read',
+  'streaming',
+  'user-read-email',
+  'user-read-private',
+];
