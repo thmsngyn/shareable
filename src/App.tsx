616a8c1f4952d08dd5cf7e38406c38eff712784a
Tom Nguyen <tomtronwin@gmail.com>
2020-04-18 16:17:46 -0700

    Add router and create a stream page

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,54 @@
 import React, { Fragment } from 'react';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import { hot } from 'react-hot-loader/root';
 
 import logo from './logo.svg';
+
+import { Spacing, FontSizes } from './styles';
 import { Login, Track, Player } from './components';
+import { Stream } from './pages';
 import { SpotifyService, CurrentPlaybackResponse, LikesResponse, SpotifyError, ItemsEntity } from './services';
 
 import './App.css';
-import { Spacing, FontSizes } from './styles';
 
 interface AppProps {}
 
 interface AppState {
-  item: any;
-  is_playing: boolean;
-  progress_ms: number;
   loggedIn: boolean;
-  likes: ItemsEntity[];
-  token: string;
 }
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
+
     this.state = {
-      token: '',
-      item: {
-        album: {
-          images: [{ url: '', height: 0, width: 0 }],
-        },
-        name: '',
-        artists: [{ name: '' }],
-        duration_ms: 0,
-      },
-      is_playing: true,
-      progress_ms: 0,
       loggedIn: false,
-      likes: [],
     };
   }
 
   componentDidMount() {
-    const token = SpotifyService.resolveUserToken();
-    if (token) {
-      this.setState({ loggedIn: true, token });
-      this.setCurrentlyPlayingState((error: SpotifyError) => {
-        this.setState({ loggedIn: false });
-      });
-      this.setLikesState((error: SpotifyError) => {
-        this.setState({ loggedIn: false });
-      });
-    } else {
-      this.setState({ loggedIn: false });
-    }
-  }
-
-  async setCurrentlyPlayingState(onError: Function) {
-    const currentlyPlaying: CurrentPlaybackResponse = await SpotifyService.getCurrentlyPlaying();
-    const { error } = currentlyPlaying;
-
-    if (error) {
-      return onError(error);
-    }
-
-    this.setState({
-      item: currentlyPlaying.item,
-      is_playing: currentlyPlaying.is_playing,
-      progress_ms: currentlyPlaying.progress_ms,
-    });
-  }
-
-  async setLikesState(onError: Function) {
-    const likes: LikesResponse = await SpotifyService.getLikes();
-    const { error } = likes;
-
-    if (error) {
-      return onError(error);
-    }
-
-    console.log(likes);
-    this.setState({ likes: likes.items! });
+    const loggedIn = !!SpotifyService.resolveUserToken();
+    this.setState({ loggedIn });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div style={styles.header}>
-            <img src={logo} className="App-logo" alt="logo" />
-            <div style={FontSizes.ExtraLarge}>Shareable</div>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <div style={styles.header}>
+              <img src={logo} className="App-logo" alt="logo" />
+              <div style={FontSizes.ExtraLarge}>Shareable</div>
+            </div>
+            <div style={styles.appContainer}>
+              {!this.state.loggedIn && <Login></Login>}
+              {this.state.loggedIn && <Route path="/" component={Stream}></Route>}
+            </div>
           </div>
-          <div style={styles.appContainer}>
-            {!this.state.loggedIn && <Login></Login>}
-            {this.state.loggedIn && (
-              <Fragment>
-                <div style={styles.section}>
-                  <div style={FontSizes.Large}>Player</div>
-                  <Player />
-                </div>
-                <div style={FontSizes.Large}>Currently playing</div>
-                <Track item={this.state.item} is_playing={this.state.is_playing} progress_ms={this.state.progress_ms} />
-                <div style={FontSizes.Large}>Likes</div>
-                {this.state.likes.map((like, index) => {
-                  return (
-                    <Track
-                      key={index}
-                      item={like.track}
-                      is_playing={this.state.is_playing}
-                      progress_ms={this.state.progress_ms}
-                    />
-                  );
-                })}
-              </Fragment>
-            )}
-          </div>
-        </header>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
