2041b2f2afdf6a6e34c8d2372fa2c8f73a09d8fc
Tom Nguyen <tomtronwin@gmail.com>
2020-04-17 22:49:58 -0700

    Add standard styles

---
+++ src/styles/index.tsx
@@ -0,0 +1,2 @@
+export * from './shared-spacing';
+export * from './shared-text';
---
+++ src/styles/shared-text.tsx
@@ -0,0 +1,30 @@
+export const FontSizes = {
+  ExtraLarge: {
+    fontSize: 48,
+    lineHeight: '54px',
+  },
+  Large: {
+    fontSize: 36,
+    lineHeight: '54px',
+  },
+  MediumLarge: {
+    fontSize: 28,
+    lineHeight: '36px',
+  },
+  Medium: {
+    fontSize: 20,
+    lineHeight: '30px',
+  },
+  MediumSmall: {
+    fontSize: 14,
+    lineHeight: '20px',
+  },
+  Small: {
+    fontSize: 12,
+    lineHeight: '18px',
+  },
+  ExtraSmall: {
+    fontSize: 11,
+    lineHeight: '16px',
+  },
+};
