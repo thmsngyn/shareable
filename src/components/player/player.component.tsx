5f2b176ceceea322c1723ca654146b930038ff36
Tom Nguyen <tomtronwin@gmail.com>
2020-04-17 22:58:00 -0700

    Display likes and currently played on top

--- src/components/player/player.component.tsx
+++ src/components/player/player.component.tsx
@@ -1,31 +1,67 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './player.css';
+import './player.scss';
+import { Spacing, FontSizes } from '../../styles';
 
-export const Player = (props: any) => {
-  const backgroundStyles = {
-    backgroundImage: `url(${props.item.album.images[0].url})`,
-  };
+interface PlayerProps {
+  item: any;
+  is_playing: any;
+  progress_ms: any;
+}
 
-  const progressBarStyles = {
-    width: (props.progress_ms * 100) / props.item.duration_ms + '%',
-  };
+export class Player extends React.Component<PlayerProps> {
+  backgroundStyles() {
+    return {
+      backgroundImage: `url(${this.props.item.album.images[0].url})`,
+      width: 500,
+      height: 500,
+    };
+  }
 
-  return (
-    <div className="App">
-      <div className="main-wrapper">
-        <div className="now-playing__img">
-          <img src={props.item.album.images[0].url} />
-        </div>
-        <div className="now-playing__side">
-          <div className="now-playing__name">{props.item.name}</div>
-          <div className="now-playing__artist">{props.item.artists[0].name}</div>
-          <div className="now-playing__status">{props.is_playing ? 'Playing' : 'Paused'}</div>
-          <div className="progress">
-            <div className="progress__bar" style={progressBarStyles} />
-          </div>
-        </div>
-        <div className="background" style={backgroundStyles} />{' '}
+  progressBarStyles() {
+    return {
+      width: (this.props.progress_ms * 100) / this.props.item.duration_ms + '%',
+    };
+  }
+
+  coverArtStyle() {
+    return {
+      backgroundImage: `url(${this.props.item.album.images[0].url})`,
+    };
+  }
+
+  render() {
+    return (
+      <div className="track" style={styles.track}>
+        {this.props.item ? (
+          <Fragment>
+            <div className="track__cover-art">
+              <img className="art" src={this.props.item.album.images[0].url} />
+            </div>
+            <div className="track__content">
+              <div style={styles.artistName}>{this.props.item.artists[0].name}</div>
+              <div className="">{this.props.item.name}</div>
+              <div style={styles.progress} className="progress">
+                <div className="progress__bar" style={this.progressBarStyles()} />
+              </div>
+            </div>
+          </Fragment>
+        ) : (
+          <div>Not playing</div>
+        )}
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const styles: Record<any, React.CSSProperties> = {
+  progress: {
+    marginTop: Spacing.s16,
+  },
+  artistName: {
+    ...FontSizes.MediumSmall,
+  },
+  track: {
+    marginBottom: Spacing.s24,
+  },
 };
