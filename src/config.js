7a2272c85b9d113a0df6d008b62e956fc91011ce
Tom Nguyen <tomtronwin@gmail.com>
2020-04-18 00:34:04 -0700

    Add web playback sdk

--- src/config.js
+++ src/config.js
@@ -1,4 +1,12 @@
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = '05b0a9e4fb784b1c866f6235ae139c3a';
 export const redirectUri = 'http://localhost:3000/redirect';
-export const scopes = ['user-top-read', 'user-read-currently-playing', 'user-read-playback-state', 'user-library-read'];
+export const scopes = [
+  'user-top-read',
+  'user-read-currently-playing',
+  'user-read-playback-state',
+  'user-library-read',
+  'streaming',
+  'user-read-email',
+  'user-read-private',
+];
