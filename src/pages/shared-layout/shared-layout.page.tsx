f13f135b599bc24dab8e6fd6b67803553bac169d
Tom Nguyen <tomtronwin@gmail.com>
2020-04-21 11:03:19 -0700

    Add lottie animation capabilities and use animations on load and error

--- src/pages/shared-layout/shared-layout.page.tsx
+++ src/pages/shared-layout/shared-layout.page.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 import { Section } from '../../components';
 
+// TODO: Move somewhere else?
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'lottie-player': any;
+    }
+  }
+}
+
 export interface SharedLayoutProps {
   hasError: boolean;
   isLoading: boolean;
@@ -12,10 +21,6 @@ export interface SharedLayoutState {}
  * Responsible for shared page layout and states
  */
 export class SharedLayout extends React.Component<SharedLayoutProps, SharedLayoutState> {
-  constructor(props: any) {
-    super(props);
-  }
-
   componentDidMount() {}
 
   render() {
@@ -23,13 +28,44 @@ export class SharedLayout extends React.Component<SharedLayoutProps, SharedLayou
 
     if (hasError) {
       return (
-        <Section headerText={'Something bad happened...'} subText={'Please refresh the page and try again.'}></Section>
+        <Section headerText={'Something bad happened...'} subText={'Please refresh the page and try again.'}>
+          <div style={styles.animation}>
+            <lottie-player
+              src="https://assets3.lottiefiles.com/packages/lf20_3x5I70.json"
+              background="transparent"
+              speed="1"
+              style={{ width: 350, height: 'auto' }}
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+        </Section>
       );
     }
     if (isLoading) {
-      return <Section>Loading...</Section>;
+      return (
+        <Section>
+          <div style={styles.animation}>
+            <lottie-player
+              src="https://assets8.lottiefiles.com/packages/lf20_6R2HIH.json"
+              background="transparent"
+              speed="2"
+              style={{ width: 350, height: 350 }}
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+        </Section>
+      );
     }
 
     return children;
   }
 }
+
+const styles: Record<any, React.CSSProperties> = {
+  animation: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+};
