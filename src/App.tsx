cc1e78c14b1c6e2e68377c2acf2e149517fbc633
Tom Nguyen <tomtronwin@gmail.com>
2020-04-20 23:58:33 -0700

    Move fonts to root

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import './App.css';
+
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -10,8 +12,6 @@ import { Footer, Header } from './components';
 import { Home } from './pages';
 import { SpotifyService } from './services';
 import { AppRoutes } from './utils';
-
-import './App.css';
 
 interface AppProps {
   isMobile: boolean;
