f574279f920dd5b9cb432b4f12f36f298dbbce11
Tom Nguyen <tomtronwin@gmail.com>
2020-04-20 19:00:07 -0700

    Updates

---
+++ precache-manifest.7ed4ff38c87d248cda3a391b72a6082b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5021ed412309cfbb7968c3c34622de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7587840169717583a0d0",
+    "url": "/static/css/main.2c18c0b7.chunk.css"
+  },
+  {
+    "revision": "a4945a3c52eff9c8e41d",
+    "url": "/static/js/2.238b9d02.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.238b9d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7587840169717583a0d0",
+    "url": "/static/js/main.93ef8f7a.chunk.js"
+  },
+  {
+    "revision": "018b5bb68a82387e5b00",
+    "url": "/static/js/runtime-main.4b5c857e.js"
+  },
+  {
+    "revision": "bc3e3a524609135d60ea2af95f01d987",
+    "url": "/static/media/CentraNo2-Book.bc3e3a52.otf"
+  },
+  {
+    "revision": "b49c1191c0aa61a96e477e0efadd336b",
+    "url": "/static/media/CentraNo2-Medium.b49c1191.otf"
+  },
+  {
+    "revision": "c7bae4fd05693132460452e0f3062baa",
+    "url": "/static/media/badminton.c7bae4fd.png"
+  },
+  {
+    "revision": "55240fe44bb2004cbb7beea05fcc80dc",
+    "url": "/static/media/gradient-logo.55240fe4.png"
+  }
+]);
