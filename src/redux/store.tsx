7fef2ef0c47779eb396d2c650f58b2199ef52dbc
Tom Nguyen <tomtronwin@gmail.com>
2020-04-30 22:02:55 -0700

    Add react redux thunk dependency

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,18 +1,22 @@
+import { createStore, Store, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reduxThunk from 'redux-thunk';
+
 import * as AppStateTypes from 'AppStateTypes';
 
 import config from '../config';
 
-import { createStore, Store } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
 import rootReducer from './reducers';
+
+const middleware = [reduxThunk];
 
 let store: Store<AppStateTypes.ReducerState>;
 
 if (config.obfuscateDevTools) {
-  store = createStore(rootReducer);
+  store = createStore(rootReducer, applyMiddleware(...middleware));
 } else {
-  store = createStore(rootReducer, composeWithDevTools());
+  // Only allow redux dev tools in a dev environment
+  store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 }
 
 export default store;
