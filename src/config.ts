e26109b2187a426ff929b31ff6dd3e191adae13f
Tom Nguyen <tomtronwin@gmail.com>
2020-05-22 17:48:55 -0700

    Improve local config

--- src/config.ts
+++ src/config.ts
@@ -19,15 +19,12 @@ const scopes = [
 ];
 
 const dev: Partial<AppConfig> = {
-  redirectUri: 'http://localhost:3000',
   shareableServiceHostname: 'http://localhost:4000',
   obfuscateDevTools: false,
 };
 
 // All env vars need to be prefixed with 'REACT_APP_'
-// https://stackoverflow.com/questions/47561604/heroku-process-env-not-showing-config-vars
 const prod: Partial<AppConfig> = {
-  redirectUri: process.env.REACT_APP_SHAREABLE_HOSTNAME,
   shareableServiceHostname: process.env.REACT_APP_SHAREABLE_SERVICE_HOSTNAME,
   obfuscateDevTools: true,
 };
@@ -36,6 +33,7 @@ const envConfig: Partial<AppConfig> = process.env.NODE_ENV === 'production' ? pr
 
 export default {
   // Common configs
+  redirectUri: process.env.REACT_APP_SHAREABLE_HOSTNAME,
   clientId: '05b0a9e4fb784b1c866f6235ae139c3a',
   scopes,
 
